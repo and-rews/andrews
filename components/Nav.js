43967add83a3a05d2ac8d10a09bd32af8d74e25d
and-rews <oseiandrews394@gmail.com>
2023-07-18 17:03:48 +0000

    framer

--- components/Nav.js
+++ components/Nav.js
@@ -44,6 +44,13 @@ const Nav = () => {
                 href={link.path}
                 key={index}
               >
+                <div className="absolute pr-14 right-0 hidden xl:group-hover:flex">
+                  <div className="bg-white relative flex text-primary items-center p-[6px] rounded-[3px]">
+                    <div className="text-[12px] leading-none font-semibold capitalize">
+                      {link.name}
+                    </div>
+                  </div>
+                </div>
                 <div>{link.icon}</div>
               </Link>
             );
