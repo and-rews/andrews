ff415393e6b929875681c69ed57cc0b6a560ad91
and-rews <oseiandrews394@gmail.com>
2023-07-18 15:49:43 +0000

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,29 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { Sora } from "@next/font/google";
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import TopLeftImg from "../components/TopLeftImg";
+
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+
+const Layout = ({ children }) => {
+  return (
+    <div
+      className={`page bg-img text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
+      style={{
+        backgroundImage:
+          "linear-gradient(to bottom, black, rgba(0, 0, 0, 0.73)), url(./bg.jpg)",
+        backgroundPosition: "center",
+      }}
+    >
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
