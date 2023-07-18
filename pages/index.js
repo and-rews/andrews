44404cd28876f7a3c1b021c5ed32a3fa047654d9
and-rews <oseiandrews394@gmail.com>
2023-07-18 17:41:47 +0000

    framer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
+import Image from "next/image";
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+import { motion } from "framer-motion";
+
+import fadein from "../variants";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      <div>
+        <div>
+          <h1 className="h1">
+            Engineering The Future
+            <br /> <span className="text-accent">One Line At A Time.</span>
+          </h1>
+          <p>
+            Through meticulous precision and craftsmanship, I engineer the
+            future, revolutionizing industries and transforming ideas into
+            extraordinary digital experiences.
+          </p>
+        </div>
+      </div>
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;
