d467fb2edbbee3e86967020f94c68dd092d25195
and-rews <oseiandrews394@gmail.com>
2023-07-20 15:20:35 +0000

    circles

--- components/Circles.js
+++ components/Circles.js
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Circles = () => {
-  return <div>Circles</div>;
+  return (
+    <div className="w-[200px] xl:w-[200px] absolute -right-16 -bottom-2 mix-blend-color-dodge">
+      <Image
+        src={"/circles.png"}
+        width={260}
+        height={200}
+        className="w-full h-full"
+        alt="circles"
+      />
+    </div>
+  );
 };
 
 export default Circles;
